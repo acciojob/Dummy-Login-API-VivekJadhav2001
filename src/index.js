d595a89a51e7cb1a43449670b4ae495d38c9ac89
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-10-13 19:08:56 +0530

    Third commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import Form from "./components/Form";
 
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Form/>, document.getElementById("root"));
