b448ce67f615094444d32816f627b67effc87bbe
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-10-21 21:48:00 +0530

    Configured app and the rendered Login in it.

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,8 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+import Login from "./Login";
 
+function App() {
+  return <Login/>;
+}
+
+export default App;
